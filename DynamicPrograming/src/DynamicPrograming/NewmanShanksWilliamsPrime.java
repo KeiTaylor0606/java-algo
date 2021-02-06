4fe0f43565c7544062d736fea6f509e1cfba43d0
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-02-06 14:34:37 +0900

    Newman–Shanks–Williams prime

---
+++ DynamicPrograming/src/DynamicPrograming/NewmanShanksWilliamsPrime.java
@@ -0,0 +1,23 @@
+package DynamicPrograming;
+
+import java.util.Scanner;
+
+public class NewmanShanksWilliamsPrime {
+	
+	static int nswp(int n) {
+		int[] dp = new int[n + 1];
+		dp[0] = dp[1] = 1;
+		for(int i = 2; i < n + 1; i++) {
+			dp[i] = 2*dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(nswp(n));
+
+	}
+
+}
